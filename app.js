24da090fad1526a4ab5eeb8d6db6b8bbb93381a1
Ayush Chugh <ayushchugh2006@gmail.com>
2021-08-01 15:45:45 +0530

    added cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 import { router as phoneBookRoute } from './routes/phoneBook.js';
 
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use(phoneBookRoute);
 
