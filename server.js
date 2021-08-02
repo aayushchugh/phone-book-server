0c7088568314141b5b5d5927335fc31b6aa4d23f
Ayush Chugh <ayushchugh2006@gmail.com>
2021-08-02 22:49:20 +0530

    more heroku config

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
 import app from './app.js';
 
+const PORT = process.env.PORT || 8000;
+
 mongoose.connect(
 	process.env.DB,
 	{ useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false },
 	() => {
-		app.listen(8000);
+		app.listen(PORT);
 	}
 );
