210819efe64a1a16509e979bc477cdbbc989b88d
Ayush Chugh <ayushchugh2006@gmail.com>
2021-08-02 14:38:30 +0530

    logic to delete number
    - also added error handling

--- controllers/phoneBook.js
+++ controllers/phoneBook.js
@@ -1,39 +1,79 @@
 import PhoneBook from '../models/phoneBook.js';
 
 export async function getAllNumbers(req, res) {
-	const allNumbers = await PhoneBook.find();
+	try {
+		const allNumbers = await PhoneBook.find();
 
-	res.send({
-		message: 'success',
-		status: 200,
-		data: allNumbers,
-	});
+		res.send({
+			message: 'success',
+			status: 200,
+			data: allNumbers,
+		});
+	} catch (err) {
+		res.send({
+			message: 'internal server error',
+			status: 500,
+		});
+	}
 }
 
 export async function addNewNumber(req, res) {
-	const { firstName, lastName, phone, email } = req.body;
+	try {
+		const { firstName, lastName, phone, email } = req.body;
 
-	const existingNumber = await PhoneBook.findOne({ phone: phone });
+		const existingNumber = await PhoneBook.findOne({ phone: phone });
 
-	if (existingNumber) {
-		return res.send({
-			message: 'phone number already exists',
-			status: 400,
+		if (existingNumber) {
+			return res.send({
+				message: 'phone number already exists',
+				status: 400,
+			});
+		}
+
+		const newPhone = new PhoneBook({
+			firstName: firstName,
+			lastName: lastName,
+			phone: phone,
+			email: email,
+		});
+
+		newPhone.save();
+
+		res.send({
+			message: 'successfully added new number',
+			status: 201,
+			data: newPhone,
+		});
+	} catch (err) {
+		res.send({
+			message: 'internal server error',
+			status: 500,
 		});
 	}
+}
 
-	const newPhone = new PhoneBook({
-		firstName: firstName,
-		lastName: lastName,
-		phone: phone,
-		email: email,
-	});
+export async function deleteNumber(req, res) {
+	try {
+		const { id } = req.params;
 
-	newPhone.save();
+		const deletedNumber = await PhoneBook.findByIdAndDelete(id);
 
-	res.send({
-		message: 'successfully added new number',
-		status: 201,
-		data: newPhone,
-	});
+		if (!deletedNumber) {
+			return res.send({
+				message: 'contact not found',
+				status: 204,
+			});
+		}
+
+		res.send({
+			message: 'deleted successfully',
+			status: 202,
+			data: deletedNumber,
+		});
+	} catch (err) {
+		res.send({
+			message: 'internal server error',
+			status: 500,
+		});
+	}
 }
--- routes/phoneBook.js
+++ routes/phoneBook.js
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 
-import { addNewNumber, getAllNumbers } from '../controllers/phoneBook.js';
+import {
+	addNewNumber,
+	getAllNumbers,
+	deleteNumber,
+} from '../controllers/phoneBook.js';
 
 const router = Router();
 
 router.route('/get-all-numbers').get(getAllNumbers);
 
 router.route('/add-new-number').post(addNewNumber);
+
+router.route('/delete-number/:id').delete(deleteNumber);
 
 export { router };
